8ac9a1ee92b17fb91666a29d963e52d2116ccbdd
Josh Cox <josh@webhosting.coop>
2024-04-26 19:19:47 -0500

    packages

--- flake.nix
+++ flake.nix
@@ -19,7 +19,8 @@
     suffix-version = version: attrs: nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair (name + version) value) attrs;
     suffix-stable = suffix-version "-23_11";
   in {
-    defaultPackage.x86_64-linux =
+    packages.x86_64-linux.default = self.packages.x86_64-linux.nx;
+    packages.x86_64-linux.nx =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
